acc23c3d14bdcf0f1cf403c3495f808d7db90071
Kunihiko Sugiura <ksugiura.pg@gmail.com>
2018-08-01 21:47:22 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -29,9 +29,47 @@ let listStr: string[] = ['1', '2', '3'];
 let tuple: [string, number];
 tuple = ['hello', 10];
 // tuple = ['hello', '10']; // * Error
+// tuple = [10, 'hello']; // * Error
 console.log(tuple[0].length);
 // console.log(tuple[1].length); // * Error
 
+tuple[2] = 'world';
+tuple[4] = 1;
+// tuple[3] = false; // * Error
+console.log(tuple);
+// console.log(tuple[5].toString());
 
+// * Enum
+enum Color {
+    Red = 1,
+    Green = 3,
+    Blue,
+}
+let color: Color = Color.Blue;
+let colorName: string = Color[2];
+console.log(color, colorName);
+
+// * Any
+let notSure: any = 4;
+notSure = 'maybe a string instead';
+notSure = false;
+notSure = [1];
+// * list any
+let listAny: any[] = [1, true, "free"];
+
+// * Void
+let unusable: void = undefined;
+
+// return function
+function returnVoid(): void {
+    console.log("This is my warning message");
+}
+console.log(returnVoid());
+
+// ** Null and Undefined
+let u: undefined = undefined;
+let n: null = null;
+
+// ** TODO:Never
 
 
